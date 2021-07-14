9ee1f494d7ef48f80ae01fd9922834602fac64eb
ak-hub-s-web <87294987+packages-ak-hub@users.noreply.github.com>
2021-07-15 03:50:12 +0700

    Update .open_app_cmd.sh

--- .open_app_cmd.sh
+++ .open_app_cmd.sh
@@ -1,14 +1,14 @@
 #/bin/bash
 
-function open whatsapp() {
+function openwhatsapp() {
  echo "opening whatsapp" |lolcat
 am start --user 0 -n com.whatsapp/com.whatsapp.Main
 }
-function open chrome() {
+function openchrome() {
  echo "opening chrome " |lolcat
 am start --user 0 -n com.android.chrome/com.google.android.apps.chrome.Main
 }
-function open termux() {
+function opentermux() {
  echo "opening termux" |lolcat
 am start --user 0 -n com.termux/com.termux.app.TermuxActivity
 }
