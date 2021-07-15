2975e1e40cfb7f1f51148165986ab16d950b9fdb
ak-hub-s-web <87294987+packages-ak-hub@users.noreply.github.com>
2021-07-15 15:37:06 +0700

    Update .open_app_cmd.sh

--- .open_app_cmd.sh
+++ .open_app_cmd.sh
@@ -3,6 +3,24 @@
 function open() {
  echo no app found |lolcat
 }
+# open_app_cmd update
+function open-update() {
+    if [ -f "/$HOME/openapp/update.sh" ]
+    then 
+    bash /$HOME/openapp/update.sh
+    else
+    echo "Error" |lolcat
+    fi
+}
+# add app to open_app_cmd.sh
+function add-app() {
+    if [ -f "/$HOME/openapp/app.sh" ]
+    then 
+    bash /$HOME/openapp/app.sh
+    else
+    echo "Error" |lolcat
+    fi
+}
 # app whatsapp
 function openwhatsapp() {
     if [ -f "/$HOME/openapp/bin/list/whatsapp.sh" ]
